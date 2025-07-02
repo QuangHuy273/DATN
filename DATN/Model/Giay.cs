369309fdfb24341c1c72e82c1cc700b1a431c4c0
Yasaki74 <83692124+Yasakikondo74@users.noreply.github.com>
2025-07-02 06:49:42 -0700

    Add files via upload

---
+++ DATN/Model/Giay.cs
@@ -0,0 +1,224 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DATN.Model
+{
+    public class Giay
+    {
+        [Key]
+        public Guid GiayId { get; set; }
+        public Guid? ChatLieuId { get; set; }
+        public Guid? ThuongHieuId { get; set; }
+        public Guid? KieuDangId { get; set; }
+        public Guid? TheLoaiGiayId { get; set; }
+        public Guid? DeGiayId { get; set; }
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@"^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠƯàáâãèéêìíòóôõùúăđĩũơưẠ-ỹ\s0-9]+$", ErrorMessage = "Tên chỉ được chứa chữ cái tiếng Việt, số và khoảng trắng")]
+        public string TenGiay { get; set; }
+        public DateTime NgayTao { get; set; }
+        public bool TrangThai { get; set; }
+        public virtual ThuongHieu? ThuongHieu { get; set; }
+        public virtual KieuDang? KieuDang { get; set; }
+        public virtual DeGiay? DeGiay { get; set; }
+        public virtual TheLoaiGiay? TheLoaiGiay { get; set; }
+        public virtual ChatLieu? ChatLieu { get; set; }
+        public virtual ICollection<HoaDonChiTiet>? HoaDonChiTiets { get; set; } = new List<HoaDonChiTiet>();
+        public virtual ICollection<GiayDotGiamGia> GiayDotGiamGias { get; set; } = new List<GiayDotGiamGia>();
+        public virtual ICollection<GiayChiTiet> GiayChiTiets { get; set; } = new List<GiayChiTiet>();
+        public virtual ICollection<GioHangChiTiet>? GioHangChiTiets { get; set; } = new List<GioHangChiTiet>();
+    }
+    public class GiayChiTiet
+    {
+        [Key]
+        public Guid GiayChiTietId { get; set; }
+
+        [Required]
+        public Guid GiayId { get; set; }
+
+
+        public Guid? KichCoId { get; set; }
+        public Guid? MauSacId { get; set; }
+
+
+        public int SoLuongCon { get; set; }
+        public string AnhGiay { get; set; }
+        public DateTime NgayTao { get; set; }
+        public DateTime NgaySua { get; set; }
+        public float Gia { get; set; }
+        public string? MoTa { get; set; }
+        public bool TrangThai { get; set; }
+
+        public virtual Giay Giay { get; set; }
+        public virtual KichCo? KichCo { get; set; }
+        public virtual MauSac? MauSac { get; set; }
+        public virtual ICollection<Anh> Anhs { get; set; } = new List<Anh>();
+    }
+    public class GiayDotGiamGia
+    {
+        [Key]
+        public Guid GiayDotGiamGiaId { get; set; }
+        public Guid? GiamGiaId { get; set; }
+        public Guid? GiayId { get; set; }
+        public virtual Giay Giay { get; set; }
+        public virtual GiamGia GiamGia { get; set; }
+
+    }
+    public class GiamGia : IValidatableObject
+    {
+        [Key]
+        public Guid GiamGiaId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@"^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠƯàáâãèéêìíòóôõùúăđĩũơưẠ-ỹ\s0-9]+$", ErrorMessage = "Tên chỉ được chứa chữ cái tiếng Việt, số và khoảng trắng")]
+        public string TenGiamGia { get; set; }
+
+        public string? SanPhamKhuyenMai { get; set; } // ✅ cho phép null
+
+        public float PhanTramKhuyenMai { get; set; }
+
+        public DateTime NgayBatDau { get; set; }
+        public DateTime NgayKetThuc { get; set; }
+
+        public bool TrangThai { get; set; }
+
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (NgayBatDau.Date < DateTime.Now.Date)
+            {
+                yield return new ValidationResult("Ngày bắt đầu không được ở trong quá khứ.", new[] { nameof(NgayBatDau) });
+            }
+        }
+        public virtual ICollection<GiayDotGiamGia> GiayDotGiamGias { get; set; } = new List<GiayDotGiamGia>();
+
+    }
+    public class ChatLieu
+    {
+        [Key]
+        public Guid ChatLieuId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@"^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠƯàáâãèéêìíòóôõùúăđĩũơưẠ-ỹ\s0-9]+$", ErrorMessage = "Tên chỉ được chứa chữ cái tiếng Việt, số và khoảng trắng")]
+        public string TenChatLieu { get; set; }
+
+        public string MoTa { get; set; }
+
+        public bool TrangThai { get; set; }
+        public virtual ICollection<Giay> Giays { get; set; } = new List<Giay>();
+    }
+    public class TheLoaiGiay
+    {
+        [Key]
+        public Guid TheLoaiGiayId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@"^[a-zA-Z0-9\s]+$")]
+        public string TenTheLoai { get; set; }
+
+        public string MoTa { get; set; }
+
+        public bool TrangThai { get; set; }
+        public virtual ICollection<Giay> Giays { get; set; } = new List<Giay>();
+    }
+    public class DeGiay
+    {
+        [Key]
+        public Guid DeGiayId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@"^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠƯàáâãèéêìíòóôõùúăđĩũơưẠ-ỹ\s0-9]+$", ErrorMessage = "Tên chỉ được chứa chữ cái tiếng Việt, số và khoảng trắng")]
+        public string TenDeGiay { get; set; }
+
+        public string MoTa { get; set; }
+
+        public bool TrangThai { get; set; }
+        public virtual ICollection<Giay> Giays { get; set; } = new List<Giay>();
+
+    }
+    public class KieuDang
+    {
+        [Key]
+        public Guid KieuDangId { get; set; }
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@"^[a-zA-Z0-9\s]+$", ErrorMessage = "Tên không được chứa ký tự đặc biệt.")]
+        public string TenKieuDang { get; set; }
+        public string MoTa { get; set; }
+        public bool TrangThai { get; set; }
+        public virtual ICollection<Giay> Giays { get; set; } = new List<Giay>();
+    }
+    public class Anh
+    {
+        [Key]
+        public Guid AnhId { get; set; }
+        public Guid GiayChiTietId { get; set; }
+        public string DuongDan { get; set; }
+        public string TenAnh { get; set; }
+        public bool TrangThai { get; set; }
+
+
+        public Anh()
+        {
+            AnhId = Guid.NewGuid();
+            TrangThai = true;
+        }
+        public virtual GiayChiTiet GiayChiTiet { get; set; }
+
+    }
+    public class ThuongHieu
+    {
+        [Key]
+        public Guid ThuongHieuId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@"^[a-zA-Z0-9\s]+$", ErrorMessage = "Tên không được chứa ký tự đặc biệt.")]
+        public string TenThuongHieu { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string SDT { get; set; }
+
+        public string DiaChi { get; set; }
+
+        public string MoTa { get; set; }
+
+        public bool TrangThai { get; set; }
+        public virtual ICollection<Giay> Giays { get; set; } = new List<Giay>();
+    }
+    public class KichCo
+    {
+        [Key]
+        public Guid KichCoId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@"^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠƯàáâãèéêìíòóôõùúăđĩũơưẠ-ỹ\s0-9]+$", ErrorMessage = "Tên chỉ được chứa chữ cái tiếng Việt, số và khoảng trắng")]
+        public string TenKichCo { get; set; }
+        public int size { get; set; }
+        public string MoTa { get; set; }
+        public bool TrangThai { get; set; }
+        public virtual ICollection<GiayChiTiet> GiayChiTiets { get; set; } = new List<GiayChiTiet>();
+    }
+    public class MauSac
+    {
+        [Key]
+        public Guid MauSacId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@"^[a-zA-Z0-9\s]+$")]
+        public string TenMau { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public string MoTa { get; set; }
+
+        public bool TrangThai { get; set; }
+        public virtual ICollection<GiayChiTiet> GiayChiTiets { get; set; } = new List<GiayChiTiet>();
+
+    }
+}
---
+++ DATN/Model/HoaDon.cs
@@ -0,0 +1,156 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DATN.Model
+{
+    public class HoaDon
+    {
+        [Key]
+        public Guid HoaDonId { get; set; } = Guid.NewGuid();
+
+        public Guid TaiKhoanId { get; set; }
+        public Guid? KhachHangId { get; set; }
+        public Guid HinhThucThanhToanId { get; set; }
+        public Guid? VoucherId { get; set; }
+
+
+        [MaxLength(50)]
+        public string TenCuaKhachHang { get; set; }
+
+        [MaxLength(20)]
+        public string SDTCuaKhachHang { get; set; }
+
+        [MaxLength(50)]
+        public string EmailCuaKhachHang { get; set; }
+
+        public DateTime NgayTao { get; set; }
+        public DateTime NgayNhanHang { get; set; }
+
+        public float TongTienSauKhiGiam { get; set; }
+
+        public string TrangThai { get; set; }
+
+        [MaxLength(200)]
+        public string GhiChu { get; set; }
+
+        public Voucher? voucher { get; set; }
+        public TaiKhoan taiKhoan { get; set; }
+        public HinhThucThanhToan hinhThucThanhToan { get; set; }
+        public KhachHang? khachHang { get; set; }
+        public virtual ICollection<HoaDonChiTiet> HoaDonChiTiets { get; set; } = new List<HoaDonChiTiet>();
+
+    }
+    public class HoaDonChiTiet
+    {
+        [Key]
+        public Guid HoaDonChiTietId { get; set; } = Guid.NewGuid();
+
+        public Guid GiayId { get; set; }
+
+        public Guid HoaDonId { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int SoLuongSanPham { get; set; }
+
+        public decimal Gia { get; set; }
+
+        // Đánh dấu đây là hàng mua hay trả lại
+        public bool TrangThai { get; set; } = false;
+
+        // Ghi nhận thời điểm khách trả hàng (chỉ có nếu TrangThai = true)
+        public DateTime? NgayTraHang { get; set; }
+
+        // Trạng thái dòng chi tiết (nếu cần phân biệt logic nâng cao)
+        [MaxLength(20)]
+        public string? TrangThaiChiTiet { get; set; }
+
+        // Ghi chú nếu có
+        [MaxLength(255)]
+        public string? GhiChu { get; set; }
+
+        // Navigation properties
+        public virtual HoaDon HoaDons { get; set; }
+        public virtual Giay Giays { get; set; }
+    }
+    public class GioHangChiTiet
+    {
+        [Key]
+        public Guid GioHangChiTietId { get; set; }
+
+        public Guid GioHangId { get; set; }
+
+        public Guid GiayId { get; set; }
+
+        public int SoLuongSanPham { get; set; }
+
+
+        public decimal Gia { get; set; }
+
+        public decimal ThanhTien => Gia * SoLuongSanPham;
+
+        public DateTime NgayTao { get; set; } = DateTime.Now;
+
+        public DateTime NgayCapNhat { get; set; } = DateTime.Now;
+
+        public bool TrangThai { get; set; }
+        public virtual GioHang GioHang { get; set; }
+        public virtual Giay Giays { get; set; }
+    }
+    public class GioHang
+    {
+        [Key]
+        public Guid GioHangId { get; set; }
+        public DateTime NgayTaoGioHang { get; set; }
+        public DateTime NgayCapNhatCuoiCung { get; set; }
+        public bool TrangThai { get; set; }
+        public Guid KhachHangId { get; set; }
+        public virtual KhachHang KhachHang { get; set; }
+        public virtual ICollection<GioHangChiTiet> GioHangChiTiets { get; set; } = new List<GioHangChiTiet>();
+
+    }
+    public class HinhThucThanhToan
+    {
+
+        [Key]
+        public Guid HinhThucThanhToanId { get; set; }
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@"^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠƯàáâãèéêìíòóôõùúăđĩũơưẠ-ỹ\s0-9]+$", ErrorMessage = "Tên chỉ được chứa chữ cái tiếng Việt, số và khoảng trắng")]
+        public string TenHinhThuc { get; set; }
+        public string MoTa { get; set; }
+        public virtual ICollection<HoaDon> HoaDons { get; set; } = new List<HoaDon>();
+    }
+    public class Voucher
+    {
+        [Key]
+        public Guid VoucherId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string TenVoucher { get; set; }
+
+        [Required]
+        public DateTime NgayBatDau { get; set; }
+
+        [Required]
+        public DateTime NgayKetThuc { get; set; }
+
+        [Range(0, 100)]
+        public float PhanTram { get; set; }
+
+        public bool TrangThai { get; set; }
+
+        public int SoLuong { get; set; }
+
+        public Guid? IdTaiKhoan { get; set; }
+
+        public void Validate()
+        {
+            if (NgayBatDau.Date < DateTime.Today)
+                throw new Exception("Ngày bắt đầu không hợp lệ.");
+            if (NgayKetThuc <= NgayBatDau)
+                throw new Exception("Ngày kết thúc phải sau ngày bắt đầu.");
+        }
+        public virtual TaiKhoan? TaiKhoan { get; set; }
+        public virtual ICollection<HoaDon> HoaDons { get; set; } = new List<HoaDon>();
+    }
+}
