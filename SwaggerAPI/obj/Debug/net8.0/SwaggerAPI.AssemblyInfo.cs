72ca3c2ed7dc91edb0e1b507f5e0361584f7a296
Yasaki74 <83692124+Yasakikondo74@users.noreply.github.com>
2025-07-10 20:41:55 +0700

    nothing

--- SwaggerAPI/obj/Debug/net8.0/SwaggerAPI.AssemblyInfo.cs
+++ SwaggerAPI/obj/Debug/net8.0/SwaggerAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwaggerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d59d9343ddca698764ff137db7dd726c8797457")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ea216682631227e0eee11218bc1ed34514edbf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwaggerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwaggerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
